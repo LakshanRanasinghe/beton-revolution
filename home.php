6df19b6dbfdfbc7d13beb33a1df9a27da85e7657
DayZ Solutions <dayzsolutions@192.168.1.11>
2024-11-18 14:59:16 +0530

    home page hero section done

---
+++ home.php
@@ -0,0 +1,39 @@
+<?php 
+/* Template Name: HomePage */ 
+
+get_header(); 
+?>
+
+<div class="bg-image w-100 object-fit-cover position-relative" style="background-image: url('https://images.unsplash.com/photo-1541888946425-d81bb19240f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); height: 90vh;">
+    <div class="z-0 hero-section-overlay position-absolute h-100 w-100 bg-black bg-opacity-50 top-0 left-0"></div>
+    <div class="container h-100 d-flex flex-column justify-content-center">
+        <div class="z-1 position-relative p-2 d-flex mb-2" style="backdrop-filter: blur(25px); background-color: #FFFFFF1A; width: 300px;">
+            <div class="container d-flex flex-row align-items-center gap-2 w-auto">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="22.5" viewBox="0 0 576 512"><path fill="#FFD43B" d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg>
+            </div>
+            <h2 class="oswald-600 fs-5 lh-sm text-white m-0" style="width: 150px;">4.9/5 Stars</h2>
+        </div> 
+
+        <div class="z-1 position-relative p-2 d-flex w-100 mb-2" style="backdrop-filter: blur(25px); background-color: #FFFFFF1A;">
+            <h1 class="oswald-500 display-3 text-56 m-0 text-white text-uppercase">Pouring Concrete?</h1>
+        </div>
+
+        <div class="z-1 position-relative p-2 w-100 mb-2" style="backdrop-filter: blur(25px); background-color: #FFFFFF1A;">
+            <h1 class="oswald-500 display-3 text-76 m-0 text-white text-uppercase">Calculate your price here</h1>
+            <div style="background: linear-gradient(90deg, #FDD401 0%, rgba(253, 212, 1, 0) 100%); height: 8px; width: 100%;"></div>
+        </div>
+
+        <div class="z-1 position-relative">
+            <button class="oswald-600 text-20 text-white text-uppercase border-2 border-white rounded-0 bg-transparent px-4 py-3">Calculate Price</button>
+            <button class="oswald-600 text-20 text-white text-uppercase border-2 border-orange bg-orange rounded-0 px-6 py-3">Free Quote</button>
+        </div>
+    </div>
+</div>
+
+<?php
+get_footer();
+?>
