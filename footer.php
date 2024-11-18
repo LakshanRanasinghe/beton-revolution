65a40d1b1772459cfbb425a87aca4ff3fa4ca2f7
LakshanRanasinghe <lakshan20b@gmail.com>
2024-11-18 12:44:12 +0530

    site footer updated

--- footer.php
+++ footer.php
@@ -12,19 +12,44 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'beton' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'beton' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'beton' ), 'beton', '<a href="https://dayzsolutions.com">DayZ Solutions</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="beton-footer-wrapper dark-blue container">
+			<div class="footer-top d-flex">
+				<div class="footer-section">
+					<P class="oswald-500 text-uppercase">About us</P>
+					<hr class="border yellow-gradient border-1">
+					<div class="footer-section-content">
+						<p class="poppins-400">
+						Want to have concrete poured cheaply or order concrete online? Then you should go to Beton Storten.nl. We are at your service on working days and throughout the Netherlands. You will receive a quote within 24 hours. We have developed a calculator so that you can request a price for your work in 3 steps.
+						</p>
+					</div>
+				</div>
+				<div class="footer-section">
+					<P class="oswald-500 text-uppercase">Services</P>
+					<hr class="border yellow-gradient border-1">
+				</div>
+				<div class="footer-section">
+					<P class="oswald-500 text-uppercase">POURING CONCRETE</P>
+					<hr class="border yellow-gradient border-1">
+				</div>
+				<div class="footer-section">
+					<P class="oswald-500 text-uppercase">Contact US</P>
+					<hr class="border yellow-gradient border-1">
+				</div>
+			</div>
+			<div class="site-info">
+				<!-- <a href="<?php //echo esc_url( __( 'https://wordpress.org/', 'beton' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					// printf( esc_html__( 'Proudly powered by %s', 'beton' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					// printf( esc_html__( 'Theme: %1$s by %2$s.', 'beton' ), 'beton', '<a href="https://dayzsolutions.com">DayZ Solutions</a>' );
+					?> -->
+			</div><!-- .site-info -->
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
