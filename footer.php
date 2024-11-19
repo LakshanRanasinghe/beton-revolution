9ed4318ecb438d7bf14b5edf939890a72738b6c3
LakshanRanasinghe <lakshan20b@gmail.com>
2024-11-19 10:26:31 +0530

    footer updated

--- footer.php
+++ footer.php
@@ -16,31 +16,34 @@
 			<div class="footer-logo pb-5">
 				<a><?php echo get_custom_logo(); ?></a>
 			</div>
-			<div class="footer-top d-sm-flex pb-4">
-				<div class="footer-section sm-col-4 pe-5">
+			
+			<!-- Top Footer Section -->
+			<div class="footer-top row pb-4">
+				<div class="footer-section col-lg-4 col-md-12 mb-4 pe-lg-5">
 					<?php 
 						if (is_active_sidebar('footer-widget-1')) :
 							dynamic_sidebar('footer-widget-1'); 
 						endif; 
 					?>
 				</div>
-				<div class="col-8">
+
+				<div class="col-lg-8 col-md-12">
 					<div class="row">
-						<div class="footer-section col-sm-4 pe-5 text-uppercase">
+						<div class="footer-section col-lg-4 col-md-4 col-sm-6 mb-4 pe-lg-5 text-uppercase">
 							<?php 
 								if (is_active_sidebar('footer-widget-2')) :
 									dynamic_sidebar('footer-widget-2'); 
 								endif; 
 							?>
 						</div>
-						<div class="footer-section col-sm-4 pe-5 text-uppercase">
+						<div class="footer-section col-lg-4 col-md-4 col-sm-6 mb-4 pe-lg-5 text-uppercase">
 							<?php 
 								if (is_active_sidebar('footer-widget-3')) :
 									dynamic_sidebar('footer-widget-3'); 
 								endif; 
 							?>
 						</div>
-						<div class="footer-section col-sm-4 text-uppercase">
+						<div class="footer-section col-lg-4 col-md-4 col-sm-12 text-uppercase">
 							<?php 
 								if (is_active_sidebar('footer-widget-4')) :
 									dynamic_sidebar('footer-widget-4'); 
@@ -51,24 +54,32 @@
 				</div>
 			</div>
 		</div>
+
 		<div class="site-info border-top-gray">
-			<div class="d-flex justify-content-between py-4 container light-blue text-15 poppins-400 align-items-center">
-				<div class="col d-flex"><p class="mb-0"><?php _e('Copyright © 2024', 'beton'); ?></p>&nbsp<a href="<?php echo site_url(); ?>" class="light-blue link-hover">Betonstorten.nl</a></div>
-				<div class="col">
-					<div class="d-flex width-219 float-end align-items-center">
-						<p class="mb-0"><?php _e('Follow Us:', 'beton'); ?>&nbsp&nbsp</p>
-						<div class="col text-center">
-							<a href="#"><img class="width-30" src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/Screenshot-2024-11-18-at-1.59.37-PM.png" alt=""></a>
-						</div>
-						<div class="col text-center">
-							<a href="#"><img class="width-30" src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/Screenshot-2024-11-18-at-1.59.37-PM.png" alt=""></a>
-						</div>
-						<div class="col text-center">
-							<a href="#"><img class="width-30" src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/Screenshot-2024-11-18-at-1.59.37-PM.png" alt=""></a>
+			<div class="container py-4 text-center text-sm-start light-blue text-15 text-sm-14 poppins-400">
+				<div class="row justify-content-between align-items-center">
+					<div class="col-12 col-sm d-flex justify-content-center justify-content-sm-start align-items-center mb-2 mb-sm-0">
+						<p class="mb-0"><?php _e('Copyright © 2024', 'beton'); ?></p>
+						&nbsp;
+						<a href="<?php echo site_url(); ?>" class="light-blue link-hover">Betonstorten.nl</a>
+					</div>
+					<div class="col-12 col-sm-auto d-flex align-items-center justify-content-center justify-content-sm-end">
+						<p class="mb-0 d-none d-sm-block"><?php _e('Follow Us:', 'beton'); ?>&nbsp;</p>
+						<div class="d-flex align-items-center">
+							<a href="#" class="mx-1">
+								<img class="width-30" src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/Screenshot-2024-11-18-at-1.59.37-PM.png" alt="">
+							</a>
+							<a href="#" class="mx-1">
+								<img class="width-30" src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/Screenshot-2024-11-18-at-1.59.37-PM.png" alt="">
+							</a>
+							<a href="#" class="mx-1">
+								<img class="width-30" src="<?php echo site_url(); ?>/wp-content/uploads/2024/11/Screenshot-2024-11-18-at-1.59.37-PM.png" alt="">
+							</a>
 						</div>
 					</div>
 				</div>
-			</div><!-- .site-info -->
+			</div>
+			<!-- .site-info -->
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
